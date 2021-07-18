122f3df835fe83d19e66d8f482a053ccc153ff9c
Phillip Relf <z5257391@unsw.edu.au>
2021-07-18 17:06:59 +1000

    Added all operations

--- src/strategies/generic.py
+++ src/strategies/generic.py
@@ -12,15 +12,14 @@ class Generic(Strategy):
 
     def __init__(self) -> None:
         super().__init__()
+        self._state = 0
+        self._state_progress = 0
 
     def get_keywords(self) -> List[bytes]:
         pass
 
     def set_sample(self, sample: Sample) -> None:
         super().set_sample(sample)
-        
-        self._state = 0
-        self._state_progress = 0
         self._testcase = self._sample._input
 
     def get_input(self) -> bytes:
@@ -34,6 +33,34 @@ class Generic(Strategy):
             else:
                 self._state += 1
         elif self._state == 1:
-            pass
+            if self._state_progress < len(self._testcase):
+                self._state_progress += 1
+                return byteflip(self._testcase)
+            else:
+                self._state += 1
+        elif self._state == 2:
+            if self._state_progress < len(self._testcase):
+                self._state_progress += 1
+                return arithmetic(self._testcase)
+            else:
+                self._state += 1
+        elif self._state == 3:
+            if self._state_progress < len(self._testcase):
+                self._state_progress += 1
+                return interestingbytes(self._testcase)
+            else:
+                self._state += 1
+        elif self._state == 4:
+            if self._state_progress < len(self._testcase):
+                self._state_progress += 1
+                return bytedelete(self._testcase)
+            else:
+                self._state += 1
+        elif self._state == 5:
+            if self._state_progress < len(self._testcase):
+                self._state_progress += 1
+                return randominsert(self._testcase)
+            else:
+                self._state += 1
         
         return None
