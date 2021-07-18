8d7afd6d0c220b25dbeb9d1c114ae5c865dc73ad
Phillip Relf <z5257391@unsw.edu.au>
2021-07-18 16:15:55 +1000

    Changed structure to state machine

--- src/strategies/json.py
+++ src/strategies/json.py
@@ -23,8 +23,7 @@ class JsonStrategy(Strategy):
         self._bytes_list = ["%s", "a", "\0", "\n"]
         self._bad_bytes = self.generate_bad_bytes() + self._bytes_list
 
-        self._large_mutation_done = False
-        self._infinite_mutation = False
+        self._state = 0
 
         self._methods = []
         for method in json_methods:
@@ -41,7 +40,7 @@ class JsonStrategy(Strategy):
 
     def get_input(self) -> bytes:
         mutated = ""
-        if self._infinite_mutation:
+        if self._state == 4:
             mutated = self.mutate_infinitely()
         else:
             mutated = self.mutate()
@@ -54,11 +53,9 @@ class JsonStrategy(Strategy):
         except:
             self._json = {}
     
-    def mutate_infinitely(self):
-        json_copy = copy.deepcopy(self._json)
-        rand_key = choice([i for i in json_copy])
-        rand_val = json_copy[rand_key]
 
+
+    def mutate_infinitely_recursive(self, rand_val):
         min = -2147483648
         max = 2147483648
 
@@ -68,76 +65,84 @@ class JsonStrategy(Strategy):
             rand_val = self.mutate_str(rand_val)
         elif isinstance(rand_val, list):
             rand_index = randint(0, len(rand_val)-1)
-            rand_list_val = rand_val[rand_index]
-            if isinstance(rand_list_val, int):
-                rand_val[rand_index] = randint(min, max)
-            elif isinstance(rand_list_val, str):
-                rand_val[rand_index] = self.mutate_str(rand_list_val)
-        # change type too? done already using methods?
+            rand_val[rand_index] = self.mutate_infinitely_recursive(rand_val[rand_index])
+        # change type too? done already using "methods"?
 
-        json_copy[rand_key] = rand_val
+        return rand_val
+
+    def mutate_infinitely(self):
+        json_copy = copy.deepcopy(self._json)
+        rand_key = choice([i for i in json_copy])
+
+        json_copy[rand_key] = self.mutate_infinitely_recursive(json_copy[rand_key])
         return json.dumps(json_copy)
 
 
 
     def mutate(self):
-        #methods = copy.deepcopy(self._methods)
-        #Thread.get_instance()._semaphoreA.acquire()
 
         # try overflow/fmt if we haven't already
-        if self._methods != []:
+        if self._state == 0:
             copy_json = copy.deepcopy(self._json)
             method_val = self._methods.pop(0)
             for key in copy_json:
                 copy_json[key] = method_val
-            #Thread.get_instance()._semaphoreA.release()
+
+            if len(self._methods) == 0:
+                self._state = 1
+                #print("NEW STATE", self._state)
             return json.dumps(copy_json)
         else:
-            rand_index = randint(0, len(self._bytes_list)-1)
-            append_byte = self._bytes_list[rand_index]
+            append_byte = choice(self._bytes_list)
 
-            stop = False
-            for key in self._json:
-                val = self._json[key]
-                if isinstance(val, str):
-                    if len(val) < 200:
-                        stop = True
-                    self._json[key] = val + append_byte
-                elif isinstance(val, int):
-                    if val == 0:
-                        val = 1
-                    if len(str(val)) < 20:
-                        stop = True
-                    self._json[key] = val*-2
-                elif isinstance(val, list):
-                    for y in range(len(val)):
-                        if isinstance(val[y], str):
-                            if len(val[y]) < 200:
-                                stop = True
-                                val[y] = val[y] + append_byte
-                        if isinstance(val[y], int):
-                            if val[y] == 0:
-                                val[y] = 1
-                            if len(str(val[y])) < 20:
-                                stop = True
-                                val[y] = val[y]*-2
-                    if len(val) < 1000:
-                        sqrt = (100 - len(val))//2
-                        for i in range(0, sqrt):
-                            val.append("a")
-                        for i in range(sqrt, 1000):
-                            val.append(randint(-10, 10))
-            
-            if stop == False and self._large_mutation_done == False:
-                self._large_mutation_done = True
+            if self._state == 1:
+                found = False
+                for key in self._json:
+                    val = self._json[key]
+                    if isinstance(val, str):
+                        if len(val) < 200:
+                            found = True
+                        self._json[key] = val + append_byte
+                    elif isinstance(val, int):
+                        if val == 0:
+                            val = 1
+                        if len(str(val)) < 20:
+                            found = True
+                        self._json[key] = val*-2
+                    elif isinstance(val, list):
+                        for y in range(len(val)):
+                            if isinstance(val[y], str):
+                                if len(val[y]) < 200:
+                                    found = True
+                                    val[y] = val[y] + append_byte
+                            if isinstance(val[y], int):
+                                if val[y] == 0:
+                                    val[y] = 1
+                                if len(str(val[y])) < 20:
+                                    found = True
+                                    val[y] = val[y]*-2
+                        if len(val) < 1000:
+                            sqrt = (100 - len(val))//2
+                            for i in range(0, sqrt):
+                                val.append("a")
+                            for i in range(sqrt, 1000):
+                                val.append(randint(-10, 10))
+                if not found:
+                    self._state = 2
+                    #print("NEW STATE", self._state)
+            elif self._state == 2:
                 for i in range(0, 50):
                     self._json[f"add{i}"] = 'a'
                 for i in range(50, 100):
                     self._json[f"add{i}"] = randint(-10, 10)
-            elif stop == False and self._large_mutation_done:
-                self._infinite_mutation = True
+
+                self._state = 3
+                #print("NEW STATE", self._state)
+            elif self._state == 3:
                 self.reset_json()
-            #Thread.get_instance()._semaphoreA.release()
+
+                self._state = 4
+                #print("NEW STATE", self._state)
             return json.dumps(self._json)
 
     def generate_bad_bytes(self):
@@ -159,9 +164,9 @@ class JsonStrategy(Strategy):
             if random_target not in targets:
                 targets.append(random_target)
         for i in targets:
-            random_bad_byte = randint(0, len(self._bad_bytes)-1)
+            random_bad_byte = choice(self._bad_bytes)
             if i >= len(str_list):
-                str_list.append(self._bad_bytes[random_bad_byte])
+                str_list.append(random_bad_byte)
             else:
-                str_list[i] = self._bad_bytes[random_bad_byte]
+                str_list[i] = random_bad_byte
         return ret.join(str_list)
