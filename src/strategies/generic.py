b9bca40b9cd3815c135aba1a14d890c4ade52f26
Phillip Relf <z5257391@unsw.edu.au>
2021-07-18 16:54:54 +1000

    Created generic fuzzer strat

---
+++ src/strategies/generic.py
@@ -0,0 +1,39 @@
+"""
+Handles plaintext inputs.
+"""
+
+from typing import List
+from src.strategies.strategy import Strategy
+from src.samples.sample import Sample
+from src.strategies.operators import bitflip
+
+
+class Generic(Strategy):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def get_keywords(self) -> List[bytes]:
+        pass
+
+    def set_sample(self, sample: Sample) -> None:
+        super().set_sample(sample)
+        
+        self._state = 0
+        self._state_progress = 0
+        self._testcase = self._sample._input
+
+    def get_input(self) -> bytes:
+        return mutate_machine()
+
+    def mutate_machine(self):
+        if self._state == 0:
+            if self._state_progress < len(self._testcase):
+                self._state_progress += 1
+                return bitflip(self._testcase)
+            else:
+                self._state += 1
+        elif self._state == 1:
+            pass
+        
+        return None
