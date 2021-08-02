5ff25d6e710ccb8af0c55a28790d20c0a51a4b68
Jonathan Clark <jonathan.clark1@student.unsw.edu.au>
2021-08-02 15:56:41 +1000

    Added a script to make testing binaries easier.

---
+++ fuzzer-test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+test_binary() {
+    ./fuzzer $1 $1.txt > /dev/null 2>&1
+    cat bad.txt | $1 > /dev/null 2>&1
+
+    if [ $? -ne 139 ]; then
+        echo "fuzzing "$1" failed"
+    else
+        echo "fuzzing "$1" passed"
+    fi
+}
+
+test_binary "binaries/plaintext/plaintext1"
+test_binary "binaries/plaintext/plaintext2"
+test_binary "binaries/plaintext/plaintext3"
+test_binary "binaries/csv/csv1"
+test_binary "binaries/csv/csv2"
+test_binary "binaries/json/json1"
+test_binary "binaries/json/json2"
