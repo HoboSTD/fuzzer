8555241754f234b98bc55e3bc2c7d30df59fd7a6
Jonathan Clark <jonathan.clark1@student.unsw.edu.au>
2021-08-02 15:24:14 +1000

    Changed the plaintext strategy to use parameters.

--- src/strategies/plaintext.py
+++ src/strategies/plaintext.py
@@ -2,6 +2,8 @@
 Handles plaintext inputs.
 """
 
+from src.parameters.parameter import Parameter
+from src.parameters.parameter_type import get_parameter_type
 from src.strategies.overflow import Overflow
 from typing import List
 from src.strategies.strategy import Strategy
@@ -20,10 +22,10 @@ class PlainText(Strategy):
 
         lines = self._sample._input.splitlines()
 
-        self._lines: List[Overflow] = [Overflow(lines[i]) for i in range(0, len(lines))]
+        self._lines: List[Parameter] = [get_parameter_type(lines[i]) for i in range(0, len(lines))]
 
     def get_input(self) -> bytes:
-        return b"\n".join([line.get_input() for line in self._lines])
+        return b"\n".join([line.get_mutation() for line in self._lines])
 
     def get_keywords(self) -> List[bytes]:
         return [b"\n", b"\x00", b"\r\n", b"\t", b"\b", b"%s"]
