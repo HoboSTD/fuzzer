36bc1b2401acf3227317bdcd65be0f9df4521810
Jonathan Clark <jonathan.clark1@student.unsw.edu.au>
2021-08-07 20:59:48 +1000

    Changed the harness to make multiple jobs to improve the number of inputs tested per second.

--- src/fuzzer.py
+++ src/fuzzer.py
@@ -2,6 +2,7 @@
 The fuzzer.
 """
 
+from threading import Lock
 from time import time
 from src.strategies.generic import Generic
 from typing import List
@@ -28,20 +29,22 @@ class Fuzzer():
             Report("other")]
         self.start: float = time()
         self.last_report: float = time()
+        self._lock: Lock = Lock()
 
     def fuzz(self) -> bytes:
         """
         Get an input for the fuzzing.
         """
 
-        if self._stop_fuzzing:
-            return None
+        with self._lock:
+            if self._stop_fuzzing:
+                return None
 
-        input = self._generic.get_input()
-        if input != None:
-            return input
-    
-        return self._strategy.get_input()
+            input = self._generic.get_input()
+            if input != None:
+                return input
+        
+            return self._strategy.get_input()
 
     def print_reports(self):
         for report in self.reports:
@@ -72,28 +75,31 @@ class Fuzzer():
         Anaylses the output.
         """
 
-        self.ninputs += 1
-        self.report()
+        with self._lock:
+            self.ninputs += 1
+            self.report()
 
-        if returncode == 0:
-            self.reports[0].inc_count()
-        elif returncode > 0:
-            self.reports[1].inc_count()
-        elif returncode == -6:
-            self.reports[2].inc_count()
-        elif returncode == -11:
-            self._stop_fuzzing = True
+            if returncode == 0:
+                self.reports[0].inc_count()
+            elif returncode > 0:
+                self.reports[1].inc_count()
+            elif returncode == -6:
+                self.reports[2].inc_count()
+            elif returncode == -11:
+                if self._stop_fuzzing:
+                    return
+                self._stop_fuzzing = True
 
-            self.report(-1)
+                self.report(-1)
 
-            print("Found input that causes segmentation fault.")
+                print("Found input that causes segmentation fault.")
 
-            with open("bad.txt", "wb") as file:
-                file.write(input)
-        elif returncode == 124:
-            self.reports[3].inc_count()
-        else:
-            self.reports[4].inc_count()
+                with open("bad.txt", "wb") as file:
+                    file.write(input)
+            elif returncode == 124:
+                self.reports[3].inc_count()
+            else:
+                self.reports[4].inc_count()
 
 class Report():
     
--- src/harness.py
+++ src/harness.py
@@ -2,6 +2,8 @@
 Harness the.
 """
 
+from multiprocessing import cpu_count
+from typing import List
 from src.jobs.job import Job
 from src.fuzzer import Fuzzer
 
@@ -13,11 +15,15 @@ class Harness():
     def __init__(self, binary_path: str, sample_path: str) -> None:
         self._binary_path = binary_path
         self._fuzzer = Fuzzer(sample_path)
+        self._njobs = cpu_count() - 1
+        self._jobs: List[Job] = []
 
     def start(self):
         """
-        Creates a job and then makes it start fuzzing.
+        Creates njobs and then makes it start fuzzing.
         """
-        
-        job = Job(self._fuzzer, self._binary_path)
-        job.start()
+
+        for _ in range(0, self._njobs):
+            job = Job(self._fuzzer, self._binary_path)
+            job.start()
+            self._jobs.append(job)
